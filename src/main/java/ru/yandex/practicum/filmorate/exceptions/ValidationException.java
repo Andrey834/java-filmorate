1361b94001c5691a4b4259347b593f729610645c
DunaevDS <dmitryesfo@yandex.ru>
2023-07-01 13:32:01 +0500

    fix: delete unused import in ValidationException

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-import javax.naming.AuthenticationException;
-
 // наследоваться лучше от RunTimeException или от AuthenticationException ?
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
