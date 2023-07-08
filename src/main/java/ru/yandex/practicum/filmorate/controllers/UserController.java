241449256d8036e6c6475421e4a5f64bb854b10e
DunaevDS <dmitryesfo@yandex.ru>
2023-07-08 20:49:17 +0500

    refactor: all methods realizations moved from FilmController to InMemoryFilmStorage and from UserController to InMemoryUserStorage

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -14,71 +16,25 @@ import java.util.*;
 @Slf4j
 @RequestMapping("/users")
 public class UserController {
-    private int id;
-    private final Map<Integer, User> users = new HashMap<>();
+    private InMemoryUserStorage userStorage;
+
+    @Autowired
+    public UserController(InMemoryUserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
 
     @PostMapping()
     public User createUser(@Valid @RequestBody User user, HttpServletRequest request) {
-        if (user == null) throw new ValidationException("User is null");
-        validation(user);
-        user.setId(getNextId());
-        users.put(user.getId(), user);
-        log.info("Получен запрос к эндпоинту: '{} {}', Строка параметров запроса: '{}'",
-                request.getMethod(), request.getRequestURI(), request.getQueryString());
-        return user;
+        return userStorage.createUser(user,request);
     }
 
     @PutMapping()
     public User updateUser(@Valid @RequestBody User user, HttpServletRequest request) {
-        if (user == null) throw new ValidationException("User is null");
-        update(user);
-        log.info("Получен запрос к эндпоинту: '{} {}', Строка параметров запроса: '{}'",
-                request.getMethod(), request.getRequestURI(), request.getQueryString());
-        return user;
+        return userStorage.updateUser(user,request);
     }
 
     @GetMapping()
     public List<User> getUsers() {
-        return new ArrayList<>(users.values());
-    }
-
-    private int getNextId() {
-        return ++id;
-    }
-
-    private void validation(User user) {
-        if (user.getEmail() == null
-                || user.getEmail().isBlank()
-                || !isValidEmail(user.getEmail())) {
-            log.error("ValidationException: incorrect email");
-            throw new ValidationException("Incorrect email");
-        }
-        if (user.getLogin() == null
-                || user.getLogin().isBlank()
-                || user.getLogin().contains(" ")) {
-            log.error("ValidationException: incorrect login");
-            throw new ValidationException("Incorrect login");
-        }
-        if (user.getBirthday() == null
-                || user.getBirthday().isAfter(LocalDate.now())) {
-            log.error("ValidationException: Date of birth cannot be in future");
-            throw new ValidationException("Date of birth cannot be in future");
-        }
-        if (user.getName() == null || user.getName().isBlank()) user.setName(user.getLogin());
-    }
-
-    private void update(User user) {
-        if (users.containsKey(user.getId())) {
-            validation(user);
-            users.put(user.getId(), user);
-        } else {
-            log.error("ValidationException: incorrect id");
-            throw new ValidationException("Incorrect id");
-        }
-    }
-
-    private boolean isValidEmail(String email) {
-        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-        return email.matches(emailRegex);
+        return userStorage.getUsers();
     }
 }
